b73f9f8172df175da683db7ffaefe3057726a284
Yanira Morales <ylm4@njit.edu>
2016-11-16 21:38:03 -0500

    UPDATED with requested functions

--- string.php
+++ string.php
@@ -1,72 +1,112 @@
+
 <?php
-//string split
-$string= 'hi hello';
-$ar1= str_split($string);
-$ar2= str_split($string, 3);
-print_r($ar1);
-print_r($ar1, 3);
+echo '<b>string split</b><br>';
+$string= 'hi hello ';
+echo chunk_split($string, 1, ".");
 
-//string repeat
-echo str_repeat("hello", 5);
 
-//string word count
-$st = 'hello, how are you doing   today?';
-print_r(str_word_count($st, 1));
-print_r(str_word_count($st, 2));
-
-//string shuffle
-$s= 'abcdef';
-$shuffled = str_shuffle($s);
-echo $shuffled;
-
-//html special characters
+echo '<b> <br>html special chars</b>';
+echo '<br>';
 $p = 'hello';
 echo htmlspecialchars($p);
 
-//string slash
+
+echo '<b> <br>string slash </b>';
+echo '<br>';
 $strr= 'What\'s your major?';
 echo stripslashes($strr);
 
 
-//string pad
-$l= 'Abel';
-echo str_pad($l, 20, 'Vega');
-
-//implode
+echo '<b> <br>implode </b>';
+echo '<br>';
 $ary= array('lastname','email','phone');
 $seper = implode('.', $ary);
 echo $seper;
 
-//print
-print('HOW ARE YOU');
 
-//string position
+echo '<b> <br>string position </b>';
+echo '<br>';
 $t='abcdef';
 $find='d';
 $pos = strpos($t,$find);
 echo $pos;
 
-//stristr()
-$web = 'www.njit.com';
-echo stristr($web, 'j');
 
-//explode
+echo '<b> <br>string length function </b>';
+echo'<br>'; 
+$string = '0123456789';
+echo strlen($string);
+
+
+echo '<b><br>Explode</b>';
+echo '<br>';
 $ar = 'arOne arTwo arThree arFour';
 $break = explode(' ',$ar);
 echo $break[1]; // arTwo
 echo $break[3];
 
-//join 
-$array = array('1', '2', '3', '4');
-echo join('<br>', $array);
 
-//sprint f--returns formated string 
+echo '<b> <br>add slash </b>';
+echo '<br>';
+$r = "Hello,  Welcome!";
+echo addslashes($r);
 
-$coursenum = 218;
-$name = 'Information system';
-$sentence = 'Take an %s class section %s';
-echo sprintf($sentence,$name,$coursenum);
 
-//string reverse
-echo strrev('NJIT')
+echo '<b><br>strip tags </b>';
+echo '<br>';
+$t= '<p>Loading...</p><a href="">Refresh Here</a>';
+echo strip_tags($t);
+echo strip_tags($t, '<p><a>');
+
+
+
+echo '<b><br>md5</b>';
+echo '<br>';
+$str = 'apple';
+if (md5($str) === '1f3870be274f6c49b3e31a0c6728957f') {
+echo "Would you like green or red apple?";
+}
+
+
+echo '<b><br>rtrim</b>';
+echo '<br>';
+$text = "\t\tThese are a few words :) ...  ";
+$binary = "\x09Example string\x0A";
+$hello  = "Hello World";
+var_dump($text, $binary, $hello);
+print "\n";
+$trimmed = rtrim($text);
+var_dump($trimmed);
+$trimmed = rtrim($text, " \t.");
+var_dump($trimmed);
+$trimmed = rtrim($hello, "Hdle");
+var_dump($trimmed);
+$clean = rtrim($binary, "\x00..\x1F");
+var_dump($clean);
+
+
+echo '<b><br> htmlentities</b><br>';
+$x = "are you <b>MAD</b>?";
+echo htmlentities($x);
+
+echo '<b><br>ECHO<br></b>';
+echo 'This is an echo test.......';
+
+echo '<b><br>Count chars<br></b>';
+$str = 'hello world!';
+echo count_chars ($str,3);
+
+
+echo '<b><br>chr<br></b>';
+$str = "The string ends in escape: ";
+$str .= chr(27); /* add an escape character at the end of $str */
+$str = sprintf("The string ends in escape: %c", 27);
+
+
+echo '<b><br>string get csv file </b><br>';
+$file = fopen('csvfile.csv','r');
+while(!feof($file)){
+echo(str_getcsv($file,','));
+}
 ?>
+
